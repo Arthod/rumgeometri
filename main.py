1df0e5be7eea3c52b18d73084a7f592c263f75d2
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2019-01-03 12:20:23 +0100

    Init

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 12:18:36 2019
+
+@author: Ahmad
+"""
+
---
+++ rumgeom.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 12:14:18 2019
+
+@author: Ahmad
+"""
+
